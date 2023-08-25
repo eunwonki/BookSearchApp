ac0dab37366346b80e1a747247bca7d20cec6d15
Eun Wonki <38225877+eunwonki@users.noreply.github.com>
2023-08-25 09:22:30 +0900

    Initial commit

---
+++ BookSearchApp/BookSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchAppApp.swift
+//  BookSearchApp
+//
+//  Created by wonki on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
